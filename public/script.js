a7a7c3556de074d863eb9f5bde6d3532afa8c184
Annika <annikarasmussen93@gmail.com>
2025-11-05 12:58:34 +0100

    sidste små ting, 10/10 frontend

--- public/script.js
+++ public/script.js
@@ -5,10 +5,36 @@ async function getHeroes() {
     const data = await response.json()
 
     data.forEach((hero) => {
-        const li = document.createElement("li")
-        li.textContent = `${hero.name} (${hero.powerstats})`;
-        list.appendChild(li)
+        const div = document.createElement("div");
+        div.classList.add("hero-card");
+
+        div.innerHTML = `
+        <img src="${hero.image}" alt="${hero.name}" class="hero-img">
+        <h3>${hero.id}. ${hero.name}</h3>
+        <p>(Real name: ${hero.fullName})</p>
+        <p>(Intelligence: ${hero.powerstats.intelligence})</p>
+        <p>(Strength: ${hero.powerstats.strength})</p>
+        <p>(Speed: ${hero.powerstats.speed})</p>
+        <p>(Durability: ${hero.powerstats.durability})</p>
+        <p>(Combat: ${hero.powerstats.combat})</p>
+            `;
+        list.appendChild(div);
     });
+
+    // data.forEach((hero) => {
+    //     const li = document.createElement("li")
+    //     li.textContent = `
+    //     ${hero.id}.
+    //     ${hero.name}
+    //     (Real name:${hero.fullName})\n
+    //     (Intelligence: ${hero.powerstats.intelligence})
+    //     (Strength: ${hero.powerstats.strength})
+    //     (Speed: ${hero.powerstats.speed})
+    //     (Durability: ${hero.powerstats.durability})
+    //     (Power: ${hero.powerstats.power})
+    //     (Combat: ${hero.powerstats.combat})`;
+    //     list.appendChild(li)
+    // });
 }
 
 getHeroes();
--- test/validateFavHero.test.js
+++ test/validateFavHero.test.js
@@ -1,13 +1,44 @@
 import {describe, it, expect} from "vitest";
-import express from "express";
+import express, {response} from "express";
+import request from "supertest";
 
 const app = express();
 app.use(express.json());
 
 
-const { validateFavorite } = require("../middlewares/validateFavorite");
+const { validateFavorite } = require("../middleware/validateFavHero");
 
 app.post("/test", validateFavorite, (req, res) => {
-    res.status(200).json({ message: "test test test test ok" });
+    res.status(200).json({ success: true});
 });
 
+
+describe("favorite validator", () => {
+    it("Should accept data", async () => {
+        const response = await request(app)
+            .post("/test")
+            .send({id: 1, note: "test note 1"});
+
+        expect(response.status).toBe(200);
+        expect(response.body.success).toBe(true);
+    });
+
+    it("No id", async () => {
+        const response = await request(app)
+            .post("/test")
+            .send({note: "test note 1"});
+
+        expect(response.status).toBe(400);
+        expect(response.body.error).toContain("id");
+    });
+
+    it("no note", async () => {
+        const response = await request(app)
+            .post("/test")
+            .send({id: 1});
+
+        expect(response.status).toBe(400);
+        expect(response.body.error).toContain("note");
+    });
+
+});
